5b8344aa3e78d152205dcbeb7e4fc37efca5689c
Yakovishchuk Maxym <yakovishchuk2004@gmail.com>
2023-02-15 15:12:58 +0200

    modified:   css/style.css 	modified:   index.html 	new file:   pages/food-1.html 	new file:   pages/reg.html 	modified:   php/registration.php  Untracked files: 	sql/

--- php/registration.php
+++ php/registration.php
@@ -3,7 +3,7 @@
 	// чистка змінних від лишнього
 	$login = filter_var(trim($_POST['login']) 
 	FILTER_SANITAZE_STRING);
-	$name = filter_var(trim($_POST['name'])
+	$name = filter_var(trim($_POST['mail'])
 	FILTER_SANITAZE_STRING);
 	$pass = filter_var(trim($_POST['pass'])
 	FILTER_SANITAZE_STRING);
@@ -14,14 +14,14 @@
 		echo "Недопустима довжина логіна";
 		exit();
 	}
-	else if (mb_strlen($name) < 3 || mb_strlen($name) > 50)
+	else if (mb_strlen($mail) < 9 || mb_strlen($mail) > 50)
 	{
-		echo "Недопустима довжина імені";
+		echo "Недопустима довжина пошти";
 		exit();
 	}
-	else if (mb_strlen($pass) < 4 || mb_strlen($pass) > 12)
+	else if (mb_strlen($pass) < 8 || mb_strlen($pass) > 16)
 	{
-		echo "Недопустима довжина пароля (від 4 до 12 символів)"
+		echo "Недопустима довжина пароля (від 8 до 16 символів)"
 		exit(); 
 	}
 	// хеш
@@ -29,8 +29,8 @@
 	
 	// Підключка до sql
 	$mysql = new mysqli('localhost','root','root','register-bd');
-	$mysql->query("INSERT INTO `users`(`login`, `pass`, `name`) 
-	VALUES('$login', '$pass', '$name')");
+	$mysql->query("INSERT INTO `users`(`login`, `pass`, `mail`) 
+	VALUES('$login', '$pass', '$mail')");
 	$mysql->close();
 	header('Location: /');
 
