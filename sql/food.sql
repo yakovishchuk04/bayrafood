bf54536aaf8ee9ff55b7e3d099e59b7791895476
yakovishchuk04 <yakovishchuk2004@gmail.com>
2023-02-19 09:00:51 +0200

    new file:   sql/food.sql

---
+++ sql/food.sql
@@ -0,0 +1,9 @@
+use bayrafood-reg;
+
+create table loginmodule(
+id int not NULL auto_increment,
+name varchar(25) not NULL,
+price float (10) not NULL,
+desc varchar (200) not NULL,
+primary key (id)
+);
