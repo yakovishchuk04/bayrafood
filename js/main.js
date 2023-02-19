c3154e2cd18c6ab16720732a8a2f452966d5a2fc
yakovishchuk04 <yakovishchuk2004@gmail.com>
2023-02-19 08:58:36 +0200

    modified:   css/style.css 	modified:   index.html 	modified:   js/main.js  Untracked files: 	sql/food.sql

--- js/main.js
+++ js/main.js
@@ -5,7 +5,20 @@ $(function(){
         slidesToShow: 3,
         slidesToScroll: 1,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.png" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.png" alt=""></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.png" alt=""></button>',
+				responsive: [{
+          breakpoint: 950,
+          settings: {
+						slidesToShow: 2,
+          }
+				},
+				{
+				breakpoint: 550,
+				settings: {
+					slidesToShow: 1,
+				}
+		}]
+				
     });     
       $('.review-title').on('click', function(){
          $('.review-text').toggleClass('review-text--active'),
